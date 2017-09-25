a0920f39a1a08dde594119772b5eba46d3080a7d
Israel Mesa <mesa.israel50@gmail.com>
2017-09-25 02:38:28 +0000

    update level two

--- level2/level2.js
+++ level2/level2.js
@@ -221,3 +221,269 @@
     ]
   }];
 })();
+
+// 2.7 Tabs Inside Out
+// We just learned that cramming a bunch of logic into our HTML is kinda bad. 
+// Let's start the Right Way with a Controller which will control our tabs.
+
+// 1.) Create a controller named TabController.
+// 2.) An empty Controller isn't much use. 
+// Do you remember why we needed a Controller at all? That's right, we need to initialize the tab property.
+// Go ahead and add that property now, setting it to 1.
+// 3.)In order to set the current tab, we'll need a setTab method to use in our HTML.
+// It should set the tab property of TabController to a value passed in.
+// 4.) We've got a setTab method.
+// Now we need an isSet method that accepts a value and returns whether that value matches this.tab.
+
+
+
+(function() {
+  var app = angular.module('gemStore', []);
+
+  app.controller('StoreController', function(){
+    this.products = gems;
+  });
+
+  app.controller('TabController', function(){ //1.)
+    this.tab = 1; //2.)
+    this.setTab = function(selectedTab) {//.3)
+        this.tab = selectedTab;
+    };
+    this.isSet = function(givenTab) {//4.)
+        return this.tab === givenTab;
+    };
+    
+  });
+
+  var gems = [
+      {
+        name: 'Azurite',
+        description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+        shine: 8,
+        price: 110.50,
+        rarity: 7,
+        color: '#CCC',
+        faces: 14,
+        images: [
+          "images/gem-02.gif",
+          "images/gem-05.gif",
+          "images/gem-09.gif"
+        ],
+        reviews: [{
+          stars: 5,
+          body: "I love this gem!",
+          author: "joe@example.org",
+          createdOn: 1397490980837
+        }, {
+          stars: 1,
+          body: "This gem sucks.",
+          author: "tim@example.org",
+          createdOn: 1397490980837
+        }]
+      },
+      {
+        name: 'Bloodstone',
+        description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+        shine: 9,
+        price: 22.90,
+        rarity: 6,
+        color: '#EEE',
+        faces: 12,
+        images: [
+          "images/gem-01.gif",
+          "images/gem-03.gif",
+          "images/gem-04.gif",
+        ],
+        reviews: [{
+          stars: 3,
+          body: "I think this gem was just OK, could honestly use more shine, IMO.",
+          author: "JimmyDean@example.org",
+          createdOn: 1397490980837
+        }, {
+          stars: 4,
+          body: "Any gem with 12 faces is for me!",
+          author: "gemsRock@example.org",
+          createdOn: 1397490980837
+        }]
+      },
+      {
+        name: 'Zircon',
+        description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+        shine: 70,
+        price: 1100,
+        rarity: 2,
+        color: '#000',
+        faces: 6,
+        images: [
+          "images/gem-06.gif",
+          "images/gem-07.gif",
+          "images/gem-09.gif"
+        ],
+        reviews: [{
+          stars: 1,
+          body: "This gem is WAY too expensive for its rarity value.",
+          author: "turtleguyy@example.org",
+          createdOn: 1397490980837
+        }, {
+          stars: 1,
+          body: "BBW: High Shine != High Quality.",
+          author: "LouisW407@example.org",
+          createdOn: 1397490980837
+        }, {
+          stars: 1,
+          body: "Don't waste your rubles!",
+          author: "nat@example.org",
+          createdOn: 1397490980837
+        }]
+      }
+    ];
+})();
+
+// 2.8 Using TabController
+// To connect our controller to our page, we're going to need a few directives.
+// Try to figure out which ones to use, but if you get stuck we'll give you hints along the way.
+// We've provided all the HTML for you. Try to accomplish the following tasks:
+
+// 1.) Attach the TabController to the <section> element with the .tab class. Don't forget to also add an alias.
+// 2.) Trigger the setTab method when a link inside a tab is clicked. 
+// Add this to each of the tab links. Pass in the number of the tab, in this case 1, 2 or 3.
+// 3.) Use the isSet method to show the corresponding tab. 
+// You'll need to add a directive to each of the <div> elements in .tab.
+// 4.) Add the active class to the correct <li> when each tab is clicked.
+// You'll need to add a directive to each <li> in .nav.
+// 5.) Within the Description tab, output the product's description within the <blockquote> element.
+// 6.) Within the Specs tab, output the product's shine value.
+// It should look something like this within the <blockquote> element: "Shine: 4".
+
+// 2.9 Creating Gallery Controller
+// Now that you're such a whiz with tabbed tab, can you apply what you've learned to our image gallery, too?
+// Don't worry, we'll start slow… And there's a lot more in common than you might think!
+
+// 1.) Let's start with a Controller, just like last time. Call it GalleryController.
+// 2.) Initialize a current property of GalleryController with a value of 0.
+// 3.) Add a method to GalleryController called setCurrent that accepts a value and assigns it to current. 
+// If no value is passed in, set current to 0.
+
+
+
+(function() {
+  var app = angular.module('gemStore', []);
+
+  
+
+  app.controller('StoreController', function(){
+    this.products = gems;
+  });
+  app.controller('GalleryController', function() {//1.)
+    this.current = 0;//2.)
+    this.setCurrent = function(newGallery){//3.)
+      this.current = newGallery || 0;
+    };
+  });
+
+  app.controller('TabController', function(){
+    this.tab = 1;
+
+    this.setTab = function(newValue){
+      this.tab = newValue;
+    };
+
+    this.isSet = function(tabName){
+      return this.tab === tabName;
+    };
+  });
+
+  var gems = [
+    {
+      name: 'Azurite',
+      description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+      shine: 8,
+      price: 110.50,
+      rarity: 7,
+      color: '#CCC',
+      faces: 14,
+      images: [
+        "images/gem-02.gif",
+        "images/gem-05.gif",
+        "images/gem-09.gif"
+      ],
+      reviews: [{
+        stars: 5,
+        body: "I love this gem!",
+        author: "joe@example.org",
+        createdOn: 1397490980837
+      }, {
+        stars: 1,
+        body: "This gem sucks.",
+        author: "tim@example.org",
+        createdOn: 1397490980837
+      }]
+    },
+    {
+      name: 'Bloodstone',
+      description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+      shine: 9,
+      price: 22.90,
+      rarity: 6,
+      color: '#EEE',
+      faces: 12,
+      images: [
+        "images/gem-01.gif",
+        "images/gem-03.gif",
+        "images/gem-04.gif",
+      ],
+      reviews: [{
+        stars: 3,
+        body: "I think this gem was just OK, could honestly use more shine, IMO.",
+        author: "JimmyDean@example.org",
+        createdOn: 1397490980837
+      }, {
+        stars: 4,
+        body: "Any gem with 12 faces is for me!",
+        author: "gemsRock@example.org",
+        createdOn: 1397490980837
+      }]
+    },
+    {
+      name: 'Zircon',
+      description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+      shine: 70,
+      price: 1100,
+      rarity: 2,
+      color: '#000',
+      faces: 6,
+      images: [
+        "images/gem-06.gif",
+        "images/gem-07.gif",
+        "images/gem-09.gif"
+      ],
+      reviews: [{
+        stars: 1,
+        body: "This gem is WAY too expensive for its rarity value.",
+        author: "turtleguyy@example.org",
+        createdOn: 1397490980837
+      }, {
+        stars: 1,
+        body: "BBW: High Shine != High Quality.",
+        author: "LouisW407@example.org",
+        createdOn: 1397490980837
+      }, {
+        stars: 1,
+        body: "Don't waste your rubles!",
+        author: "nat@example.org",
+        createdOn: 1397490980837
+      }]
+    }
+  ];
+})();
+
+// 2.10 Using Gallery Controller
+// Now that we've got an awesome GalleryController, let's wire it up to the page:
+
+// 1.) Attach GalleryController to the .gallery element that wraps our gallery; use the alias gallery.
+// 2.) Change the first ng-src directive to use the current property. Properly use current as the index instead of 0.
+
+
+
+
+
